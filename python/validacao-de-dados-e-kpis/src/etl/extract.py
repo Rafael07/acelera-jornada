1f3d2470ad4b80c166644190a372889fd80d961c
Rafael07 <rafaelrodriguesdsa@gmail.com>
2025-11-03 22:21:37 -0300

    fix: ajuste da lógica de extração

--- python/validacao-de-dados-e-kpis/src/etl/extract.py
+++ python/validacao-de-dados-e-kpis/src/etl/extract.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import sys
+from typing import List, Dict, Any
 
 # Adiciona o diretório src ao path do Python
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -9,13 +10,16 @@ sys.path.insert(0, src_dir)
 
 from project_config import PATHS
 
-employee_list: list = []
-csv_path = PATHS['DATA_DIR']/'funcionarios.csv'
+def extract_employees() -> List[Dict[str, Any]]:
+    employee_list: list = []
+    csv_path = PATHS['DATA_DIR']/'funcionarios.csv'
 
-with open(csv_path, mode='r', encoding='utf-8') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+    with open(csv_path, mode='r', encoding='utf-8') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-    for rows in csv_reader:
-        employee_list.append(rows)
+        for rows in csv_reader:
+            employee_list.append(rows)
+    
+    return employee_list
 
-print(employee_list)
+
